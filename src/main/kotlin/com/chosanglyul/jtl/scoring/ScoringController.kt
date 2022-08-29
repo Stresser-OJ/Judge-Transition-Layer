27649c7029a702c856f0fb816cf97f8b23b2bbcb
chosanglyul <chosanglyul@gmail.com>
2022-08-30 00:23:17 +0900

    CHOS-11 Connect Mysql

--- src/main/kotlin/com/chosanglyul/jtl/scoring/ScoringController.kt
+++ src/main/kotlin/com/chosanglyul/jtl/scoring/ScoringController.kt
@@ -2,17 +2,21 @@ package com.chosanglyul.jtl.scoring
 
 import com.chosanglyul.jtl.client.ClientSubmissionAddRequest
 import com.chosanglyul.jtl.client.Judge0Client
+import com.chosanglyul.jtl.repository.Problem
+import com.chosanglyul.jtl.repository.ProblemRepository
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
 class ScoringController(
     private val scoringService: ScoringService,
+    private val problemRepository: ProblemRepository,
 ) {
     @PostMapping("/submission")
     fun addSubmission(
@@ -26,5 +30,12 @@ class ScoringController(
         @RequestParam("token") token: String,
     ): Mono<SubmissionRunResponse> {
         return scoringService.getSubmission(token)
+    }
+
+    @GetMapping("/problems")
+    fun getProblems(
+
+    ): Flux<Problem> {
+        return problemRepository.findAll()
     }
 }
